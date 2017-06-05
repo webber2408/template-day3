7aa830288cc19e4db1231bb84df7e5149e50fc36
webber2408 <webcrat.tech@gmail.com>
2017-06-05 14:58:52 +0530

    minor changes in validation

--- js/validation.js
+++ js/validation.js
@@ -5,20 +5,29 @@ function validate_login(){
     var vpass=document.myform1.password1.value;
     var passw=  /^[A-Za-z]\w{7,14}$/; 
     console.log("hello"); 
-      if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){  
+    if(!vemail){
+      alert("Email cannot be empty !");
+    }
+    else{
+    if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){  
         alert("Please enter a valid e-mail address !");  
-        
-  	}
-	if(!vpass.match(passw))   
-	{    
-		alert('Please input correct password !');
-		  
-	}
+    }
+  }
+    if(!vpass){
+        alert("Password cannot be empty !");
+    }
+    else{
+
+    	if(!vpass.match(passw))   
+    	{    
+    		alert('Please input correct password !');
+    		  
+    	}
     else if(vpass.length < 8)
     {
     	alert('Password should be atleast 8 characters long !');
     }
-   
+   }
 }
 function validate_registeration(){
 	var vemail=document.myform.email.value;
@@ -26,23 +35,42 @@ function validate_registeration(){
 	var atposition=vemail.indexOf("@");  
     var dotposition=vemail.lastIndexOf(".");  
     var vpass=document.myform.password.value;
+    var vname = document.myform.name.value;
+    if(!vname){
+      alert("Name cannot be empty !");
+    }
+    if(!contact){
+      alert("Contact cannot be empty !");
+    }
+    else{
   	if (isNaN(contact)){  
 	  alert("Please enter only numeric contacts ! ");  
 	  //return false;  
     }
+  }
     var passw=  /^[A-Za-z]\w{7,14}$/;  
-	if(!vpass.match(passw))   
-	{    
-		alert('Please input correct password !');
-		  
-	}
-    else if(vpass.length < 8)
-    {
-    	alert('Password should be atleast 8 characters long !');
+  	if(!vpass){
+          alert("Password cannot be empty !");
+      }
+      else{
+
+            if(!vpass.match(passw))   
+            {    
+              alert('Please input correct password !');
+                
+            }
+          else if(vpass.length < 8)
+          {
+            alert('Password should be atleast 8 characters long !');
+          }
+         }
+     if(!vemail){
+      alert("Email cannot be empty !");
     }
-     if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){  
-        alert("Please enter a valid e-mail address !");  
-        
-  	}
+    else{
+          if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){  
+              alert("Please enter a valid e-mail address !");  
+          }
+        }
 }
 
